132f9ed0cc83f475359a6de299ca45c9fdac3ca2
Ivan Dyachenko <ivan.dyachenko@gmail.com>
2018-02-25 14:56:07 +0100

    Update version to 1.0.3

--- version.rb
+++ version.rb
@@ -2,18 +2,18 @@ require 'rbconfig'
 class Version < Formula
   desc ""
   homepage "https://github.com/ivan-dyachenko/version"
-  version "1.0.2"
+  version "1.0.3"
 
   if Hardware::CPU.is_64_bit?
     case RbConfig::CONFIG['host_os']
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.2/version_1.0.2_darwin_amd64.zip"
-      sha256 "4109aa18f175e97093863b2e93de4e61e03ae1085c22b85aeb6374b726e70fc1"
+      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.3/version_1.0.3_darwin_amd64.zip"
+      sha256 "ea24beaa12f75b6f925592db49e07a9cb266a41d5dc42910ea21d2b2bc5f8e2d"
     when /linux/
-      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.2/version_1.0.2_linux_amd64.tar.gz"
-      sha256 "a55e88736eb5b55353f46451a9fe50805eafa155fe8b0019d4b90ff8a11c4418"
+      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.3/version_1.0.3_linux_amd64.tar.gz"
+      sha256 "f9f43908faaa9296b52c161d89ab9f16e506d396579ad5efa6423f4c4567ea67"
     when /solaris|bsd/
       :unix
     else
@@ -24,11 +24,11 @@ class Version < Formula
     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
       :windows
     when /darwin|mac os/
-      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.2/version_1.0.2_darwin_386.zip"
-      sha256 "656a6ff3a12eb2e8d9e2bc120f9683cd51b5810c84358bfa78c420679b4dee7e"
+      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.3/version_1.0.3_darwin_386.zip"
+      sha256 "d340da50c85f9f7b5732427fb48196c7d87a18c56b4062eb4b251b451d1f3fe8"
     when /linux/
-      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.2/version_1.0.2_linux_386.tar.gz"
-      sha256 "2ef60fe21c74f6dae8485473b29b8f27668675f1872a487743ee59b97fc649ff"
+      url "https://github.com/ivan-dyachenko/version/releases/download/v1.0.3/version_1.0.3_linux_386.tar.gz"
+      sha256 "aa7387a5330ea799e069044dc868bbedb223fdfe219b1277efe8b136965aec52"
     when /solaris|bsd/
       :unix
     else
